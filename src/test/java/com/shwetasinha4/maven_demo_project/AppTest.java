93a8cf85ae7e3e4646d7ea2e5643905f173f8873
Shweta Sinha <shweta_sinha@persistent.com>
2023-06-10 10:04:46 +0530

    added initial commit

---
+++ src/test/java/com/shwetasinha4/maven_demo_project/AppTest.java
@@ -0,0 +1,20 @@
+package com.shwetasinha4.maven_demo_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
